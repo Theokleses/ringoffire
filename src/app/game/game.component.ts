1ed0ce01ce770a31005e1039a42504be9563213a
Theokleses <osahonschmolze222@gmail.com>
2025-11-13 12:28:55 +0100

    Added Game model, shuffle function, and ngOnInit game setup
    - Created /models/game.ts containing all game arrays
    - Added logic to generate and shuffle the 52-card stack
    - Implemented newGame() method and called it inside ngOnInit()
    - Used Fisher–Yates algorithm to randomize cards in the stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-
   pickCardAnimation = false;
+  game: Game = new Game();
 
-  takeCard(){
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  takeCard() {
     this.pickCardAnimation = true;
   }
 
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,25 @@
+export class Game {
+  public players: string[] = [];
+  public stack: string[] = [];
+  public plyedCards: string[] = [];
+  public currentPlayer: number[] = [];
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('ace_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+      this.stack.push('hearts_' + i);
+    }
+    this.stack = shuffle(this.stack);
+  }
+}
+
+export function shuffle<T>(array: T[]): T[] {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
