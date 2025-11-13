697f473cfb5af80b1d40f3de86c9f684adc35924
Theokleses <osahonschmolze222@gmail.com>
2025-11-13 14:24:55 +0100

    Display played cards and refine draw animation
    - Added rendering of played cards using *ngFor
    - Moved playedCards.push into the setTimeout to sync with the draw animation
    - Reduced animation timeout from 1500ms to 1000ms for smoother effect
    - Updated .played-card CSS to adjust animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { StartScreenComponent } from "../start-screen/start-screen.component";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StartScreenComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
@@ -23,15 +24,17 @@ export class GameComponent {
       this.currentCard = this.game.stack.pop() || '';
       console.log(this.currentCard);
       this.pickCardAnimation = true;
+      console.log(this.game);
+      console.log(this.currentCard);
 
       setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
-      }, 1500);
+      }, 1000);
     }
   }
 
   newGame() {
     this.game = new Game();
-    console.log(this.game);
   }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,7 +1,7 @@
 export class Game {
   public players: string[] = [];
   public stack: string[] = [];
-  public plyedCards: string[] = [];
+  public playedCards: string[] = [];
   public currentPlayer: number[] = [];
 
   constructor() {
