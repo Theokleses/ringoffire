0dcc615b7ef5b746cdd0c1de997970e5b210764a
Theokleses <osahonschmolze222@gmail.com>
2025-11-14 13:18:34 +0100

    Imported MatButtonModule and MatIconModule from Angular Material and added SCSS positioning styles

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
 import { StartScreenComponent } from "../start-screen/start-screen.component";
 import { PlayerComponent } from '../player/player.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, StartScreenComponent, PlayerComponent],
+  imports: [CommonModule, StartScreenComponent, PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
