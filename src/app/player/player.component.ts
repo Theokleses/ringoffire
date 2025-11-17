e5fca39b7e0c2af7eee207b40af60c7e5878c40f
Theokleses <osahonschmolze222@gmail.com>
2025-11-17 20:01:26 +0100

    Syntax error corrected and game component removed, as it was not necessary.

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [GameComponent],
+  imports: [],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
